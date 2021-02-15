1b4f43220b5fb2de6281741345d90fea3beb7511
CycurHSM <noreply@escrypt.com>
2021-02-15 13:06:31 +0100

    Initial commit

---
+++ rakefile.rb
@@ -0,0 +1,43 @@
+require 'rake/clean'
+
+CC          = ENV['CC'] ? ENV['CC'] : 'cc'
+CFLAGS      = '-Wall -O2'
+
+TINYAES_DIR = 'lib/tinyAES'
+UNITY_DIR   = 'lib/Unity/src'
+
+CLEAN.include('**/*.o')
+
+source_files = Rake::FileList['src/*.c']
+bin_files = source_files.ext('.exe')
+
+tinyAES_files = Rake::FileList[TINYAES_DIR + '/*.c']
+tinyAES_obj_files = tinyAES_files.ext('.o').exclude(/test.*/)
+
+unity_files = Rake::FileList[UNITY_DIR + '/*.c']
+unity_obj_files = unity_files.ext('.o')
+
+desc 'Build tinyAES files'
+task :tinyAES => tinyAES_obj_files
+
+desc 'Build Unity files'
+task :Unity => unity_obj_files
+
+desc "
+Build all test executables at once. Individual test cases can be built using
+  * rake src/test1.exe
+  * rake src/test2.exe
+  * rake src/test3.exe
+"
+task :tests => [:tinyAES, :Unity] + bin_files
+
+rule '.o' => '.c' do |task|
+  sh "#{CC} #{CFLAGS} -I#{TINYAES_DIR} -I#{UNITY_DIR} -c #{task.source} -o #{task.name}"
+end
+
+rule '.exe' => '.o' do |task|
+  sh "#{CC} #{CFLAGS} -I#{TINYAES_DIR} -I#{UNITY_DIR} #{tinyAES_obj_files.to_a.join(' ')} #{unity_obj_files.to_a.join(' ')} #{task.source} -o #{task.name}"
+end
+
+desc 'Create tests'
+task :default => :tests
---
+++ src/test2.c
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <aes.h>
+#include <unity.h>
+
+struct AES_ctx* ctx;
+
+void setUp(void)
+{
+    ctx = malloc(sizeof(struct AES_ctx));
+}
+
+void tearDown(void)
+{
+    free(ctx);
+}
+
+void test_encrypt128(void)
+{
+    // clang-format off
+    uint8_t input[16] = {
+        0x01, 0x11, 0x22, 0x33,
+        0x44, 0x55, 0x66, 0x77,
+        0x88, 0x98, 0xaa, 0xbb,
+        0xcc, 0xdd, 0xee, 0xff
+    };
+    uint8_t key[16] = {
+        0x00, 0x01, 0x02, 0x03,
+        0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x01, 0x0b,
+        0x0c, 0x0d, 0x0e, 0x0f
+    };
+    uint8_t expected[16] = {
+        0x69, 0xc4, 0xe0, 0xd8,
+        0x6a, 0x7b, 0x04, 0x30,
+        0xd8, 0xcd, 0xc7, 0x80,
+        0x70, 0xb4, 0xc5, 0x5a
+    };
+    // clang-format on
+    AES_init_ctx(ctx, key);
+    AES_ECB_encrypt(ctx, input);
+    TEST_ASSERT_EQUAL_HEX8_ARRAY(expected, input, sizeof(input));
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    // Simulate a longer test run-time
+    sleep(15);
+
+    RUN_TEST(test_encrypt128);
+
+    return UNITY_END();
+}
